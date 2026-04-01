2e9d49a788f613ea2fc9a896c2cb9dc6d373ce9c
Punith-Gowda-G <punithg@kssem.edu.in>
2026-04-01 10:01:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask import render_template
 import requests
 
-app = Flask(__name__, template_folder="template")
+app = Flask(__name__)
 
 @app.route("/posts")
 def posts():
