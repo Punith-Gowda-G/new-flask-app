a94a1fbd2fe16b5198eca79f93822fdf9215b327
Punith-Gowda-G <punithg@kssem.edu.in>
2026-04-01 11:32:58 +0530

    Refactor response format in Flask routes
    Updated response structure in posts, comments, and albums routes to include status and status_code.

--- app.py
+++ app.py
@@ -7,16 +7,24 @@ app = Flask(__name__)
 @app.route("/posts")
 def posts():
     return jsonify(requests.get("https://jsonplaceholder.typicode.com/posts").json())
-    "status": "successfully triggered auto deployment"
+        "data": response.json(),
+        "status": "successfully triggered auto deployment",
+        "status_code": 200
+    
 
 @app.route("/comments")
 def comments():
     return jsonify(requests.get("https://jsonplaceholder.typicode.com/comments").json())
-    "status": "successfully triggered auto deployment"
+        "data": response.json(),
+        "status": "successfully triggered auto deployment",
+        "status_code": 200
     
 @app.route("/albums")
 def albums():
     return jsonify(requests.get("https://jsonplaceholder.typicode.com/albums").json())
+        "data": response.json(),
+        "status": "successfully triggered auto deployment",
+        "status_code": 200
 
 
 if __name__ == "__main__":
